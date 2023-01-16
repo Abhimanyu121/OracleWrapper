510bcc222c98368eff797d20a87881f4e9cb9bc9
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-01-16 22:50:12 +0530

    initial draft ready

--- contracts/OracleWrapper.sol
+++ contracts/OracleWrapper.sol
@@ -1,9 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import "./interfaces/IBaseWrapper.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PrimaryWrapper is IBaseWrapper {
-    function getPrice(
-        bytes calldata encodedData
-    ) external override returns (uint256, uint8, uint8) {}
+contract PrimaryWrapper is Ownable {
+    struct Query {
+        IBaseWrapper module;
+        bytes queryData;
+    }
+    mapping(bytes => Query) queries;
+    mapping(address => bool) supportedModules;
+
+    event ModuleAdded(address indexed moduleAddress);
+
+    event QueryAdded(
+        address indexed moduleAddress,
+        bytes indexed queryIdentifier,
+        bytes indexed queryData
+    );
+
+    function getPrice(bytes calldata _queryData)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint8
+        )
+    {
+        Query memory query = queries[_queryData];
+        return query.module.getPrice(query.queryData);
+    }
+
+    function addQuery(
+        bytes calldata _queryIdentifier,
+        bytes calldata _queryData,
+        address moduleAddress
+    ) external onlyOwner {
+        require(supportedModules[moduleAddress], "Module address invalid");
+        Query memory query = Query(IBaseWrapper(moduleAddress), _queryData);
+        queries[_queryIdentifier] = query;
+        emit QueryAdded(moduleAddress, _queryIdentifier, _queryData);
+    }
+
+    function addModule(address moduleAddress) external onlyOwner {
+        require(moduleAddress != address(0), "Module address invalid");
+
+        supportedModules[moduleAddress] = true;
+        emit ModuleAdded(moduleAddress);
+    }
 }
+
+//oracle will have oracle data with module address
+// abi encoded data
--- contracts/interfaces/IBaseWrapper.sol
+++ contracts/interfaces/IBaseWrapper.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 interface IBaseWrapper {
     function getPrice(bytes calldata encodedData)
         external
+        view
         returns (
             uint256 price,
             uint256 timestamp,
